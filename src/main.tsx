9f995ab26534e322e1b1772cf45bf55375422433
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2026-04-08 00:17:01 -0300

    feat(analytics): add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ToastProvider } from './contexts/ToastContext.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ToastProvider>
+      <Analytics />
       <App />
     </ToastProvider>
   </StrictMode>,
